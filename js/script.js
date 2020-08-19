197b6c1256e442be361982ec87e47ee18e8baf0d
Kate <example@gmail.com>
2020-08-19 17:47:01 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 /*jshint esversion: 6 */
 //"use strict"
-const category = 'toys';
+console.log('arr' + " - object");
+console.log(4 + + " 5");
 
-console.log(`https://url.com/${category}/5`);
+let incr = 10,
+    decr = 10;
 
-const user = 'Kate';
+   // ++incr;      //incr++;
+   // --decr;      //decr--;
 
-alert(`Hey, ${user}`);
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%3);
+
+    console.log(2*4 === 8);
+
+
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
+const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 === 8);
+
+console.log(2 + 2 * 2 != 8);
 
